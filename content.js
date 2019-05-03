4e268204a3c762b4b62fe0d3b4dcc8b3b5d211ac
Robert Fuchs <RobertFuchs97@gmail.com>
2019-05-03 11:29:45 +0200

    Create script that changes the font size of all parapgraphs
    The script executes when a button is pressed and accesses the previously set variable
    to determine the new font size.

---
+++ content.js
@@ -0,0 +1,9 @@
+
+var paragraphs = document.getElementsByTagName('p');
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        for (var i = 0, l = paragraphs.length; i < l; i++) {
+            paragraphs[i].style.fontSize = request.fontSize;
+        }
+});

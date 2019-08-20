5376788b4a3d86861c95afb2239750ec36860f2d
Robert Fuchs <RobertFuchs97@gmail.com>
2019-08-20 22:37:36 +0200

    Generify initialization of buttons

--- popup.js
+++ popup.js
@@ -1,24 +1,20 @@
 'use strict';
 
 function changeFontSize(size) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { fontSize: size }, null);
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () { 
+document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.executeScript({
         file: "content.js"
     });
 
-    var button18px = document.getElementById('btn-18px');
-    var button20px = document.getElementById('btn-20px');
-    var button22px = document.getElementById('btn-22px');
-    var button24px = document.getElementById('btn-24px');
+    var buttons = document.getElementsByTagName('button');
 
-    button18px.addEventListener('click', function() { changeFontSize("18px"); }, false);
-    button20px.addEventListener('click', function() { changeFontSize("20px"); }, false);
-    button22px.addEventListener('click', function() { changeFontSize("22px"); }, false);
-    button24px.addEventListener('click', function() { changeFontSize("24px"); }, false);
+    for (var i = 0, l = buttons.length; i < l; i++) {
+        buttons[i].addEventListener('click', function() { changeFontSize(this.innerHTML); }, false);
+    }
 
 }, false);
